03c201698046359f2c84ef6360036c4a2eb155dd
SSAAMMAA <facundo.samartino@gmail.com>
2019-12-27 10:10:21 -0300

    agregado de componentes y link router

--- src/components/CustomerEdit.js
+++ src/components/CustomerEdit.js
@@ -5,7 +5,7 @@ const CustomerEdit = ({name,dni,age}) => {
     return (
         <div>
             <h2>Edicion del cliente</h2>
-            <h3>Nombre: {name} / Dni: {dni} / Edad: {age} </h3>
+            <h3>Nombre: {name} / DNI: {dni} / Edad: {age} </h3>
         </div>
     );
 };
