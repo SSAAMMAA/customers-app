6262bc148ed14e3c757a048dc90302c95a723cc7
SSAAMMAA <facundo.samartino@gmail.com>
2019-12-27 12:34:28 -0300

    prueba con router route y switch

--- src/App.js
+++ src/App.js
@@ -1,14 +1,32 @@
 import React, {Component} from 'react';
-import { Link, BrowserRouter as Router } from 'react-router-dom';
+import { Link, BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.css';
 
 class App extends Component {
+
+  renderHome = () => <h1>Home</h1>;
+
+  renderCustomerContainer = () => <h1>Customer Container</h1>;
+  
+  renderCustomerListContainer = () => <h1>Customer List Container</h1>;
+
+  renderCustomerNewContainer = () => <h1>Customer New Container</h1>;
+
+  
+  
   render(){
     return (
       <Router>
+
+        {/* Ruter solo puede tener un elemento, no puede haber por ejemplo dos div */}
         <div className="App">
-          <Link to="/customers">Customers</Link> 
-          <Link to="/customers/30000000">Customers 30000000</Link>
+          <Route exact path="/" component={this.renderHome}/>
+          <Route exact path="/customers" component={this.renderCustomerListContainer}/>
+          <Switch>
+           {/*ante la primer coincidencia valida utiliza esa  */}
+            <Route exact path="/customers/new" component={this.renderCustomerNewContainer}/>
+            <Route exact path="/customers/:dni" component={this.renderCustomerContainer}/>
+          </Switch>
         </div>
       </Router>
     );
